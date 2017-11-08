999eb22b432df5bcbfe9266631f85350b65b8db0
Anton Suprunchuk <antouhou@gmail.com>
2017-11-08 14:23:27 +0300

    Added tests for utils

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,16 @@
-function lowerBound(array, value) {
-  const closestElement = array.sort((a, b) => Math.abs(x - a) - Math.abs(x - b))[0];
-}
+/**
+ * Returns index of the first element in the array which is not less than given value.
+ * @param {Array<number>} array
+ * @param {number} value
+ * @returns {number} index
+ */
+const lowerBound = function lowerBound(array, value) {
+  const arrayCopy = array.slice(0);
+  const closestGreaterOrEqualElement = arrayCopy.sort((a, b) => Math.abs(value - a) - Math.abs(value - b))[0];
+  const index = array.indexOf(closestGreaterOrEqualElement);
+  return index;
+};
 
 module.exports = {
-  lowerBound
+  lowerBound,
 };
---
+++ test/utils.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+const { lowerBound } = require('../src/utils');
+
+describe('utils', () => {
+  describe('lowerBound', () => {
+    it('should find closest element in array to a given, when value not in array', () => {
+      const x = 8;
+      const array = [5, 10, 15, 9, 20, 25, 30, 35];
+      const boundIndex = lowerBound(array, x);
+      expect(boundIndex).to.be.a('number');
+      expect(boundIndex).to.equal(3); // index of 9
+      expect(array[boundIndex]).to.be.at.least(x);
+    });
+    it('should find closest element in array to a given when value in array', () => {
+      const x = 20;
+      const array = [10, 20, 30, 30, 20, 10, 10, 20];
+      const boundIndex = lowerBound(array, x);
+      expect(boundIndex).to.be.a('number');
+      expect(boundIndex).to.equal(1); // First encounter of 20
+      expect(array[boundIndex]).to.be.at.least(x);
+    });
+    it('should give same results as cpp implementation', () => {
+      const array = [10, 20, 30, 30, 20, 10, 10, 20];
+      array.sort((a, b) => a - b);
+      const boundIndex = lowerBound(array, 20);
+      expect(boundIndex).to.be.equal(3);
+    });
+  });
+});
